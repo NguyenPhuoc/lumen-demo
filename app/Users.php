d9639885c1f56cb63ee081433518ed4aa5c04147
Nguyễn Hữu Phước <Hailua5gs@Gmail.com>
2018-07-20 10:47:53 +0700

    //demo user model 1

--- app/Users.php
+++ app/Users.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Users extends Model
-{//demo user model
+{//demo user model 1
 
     protected $table = 'users';
 
